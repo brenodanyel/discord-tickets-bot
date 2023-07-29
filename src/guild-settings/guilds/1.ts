716d42f1c543063a7eb5a74923866e83380a3ee5
bdls <bdls1704@hotmail.com>
2023-07-28 23:31:56 -0300

    rename guild file

--- src/guild-settings/guilds/1.ts
+++ src/guild-settings/guilds/1.ts
@@ -74,5 +74,76 @@ export const GUILD_SETTINGS: GuildSettings = {
                 },
             ],
         },
+        "EN-US": {
+            CHANNEL_IDS: {
+                CREATE_NEW: "1134591969574785084",
+                OPENED_CATEGORY: "1134592285741424793",
+                CLOSED_CATEGORY: "1134592310580084917",
+                BACKUP_CHANNEL: "1134671541674725488",
+            },
+            PREDEFINED_MESSAGES: {
+                TICKET_CHANNEL_MESSAGE_CONTENT: {
+                    color: 0x0099ff,
+                    title: "SUPORTE",
+                    description: "Seja bem vindo ao suporte, selecione um assunto abaixo para que possamos te ajudar!",
+                    placeholder: "Selecione um assunto",
+                },
+                CLOSE_TICKET_MESSAGE_CONTENT: {
+                    color: 0xff0000,
+                    title: "Encerrar Ticket",
+                    description: "Caso já esteja satisfeito, você pode clicar no botão abaixo para finalizar o seu atendimento.",
+                },
+                TICKET_CREATED_MESSAGE_CONTENT: {
+                    color: 0x0099ff,
+                    description:
+                        "Olá __USER__! este é o seu ticket, faça uma breve descrição sobre o assunto que deseja tratar e aguarde por um membro da equipe.",
+                },
+                TICKET_CLOSED_MESSAGE_CONTENT: {
+                    title: "Ticket Encerrado",
+                    color: 0xff0000,
+                    description: "Este ticket foi encerrado por __USER__!",
+                },
+                TICKET_REOPENED_CONTENT: {
+                    title: "Ticket Reaberto",
+                    color: 0x0099ff,
+                    description: "Este ticket foi reaberto por __USER__!",
+                },
+                CLOSED_TICKET_MESSAGE_SENT_TO_USER: "O seu ticket foi encerrado! Caso precise de mais ajuda, abra um novo ticket.",
+                ALREADY_HAVE_A_TICKET: "Você já possui um ticket aberto sobre este assunto.",
+                TICKET_CREATED_SUCCESSFULLY: "O seu ticket foi criado com sucesso!",
+            },
+            CATEGORIES: [
+                {
+                    label: "SUPORTE TÉCNICO",
+                    description: "Selecione para receber suporte técnico",
+                    value: "ticket_suporte_tecnico",
+                    emoji: { name: "😂" },
+                },
+                {
+                    label: "DENÚNCIA",
+                    description: "Selecione para fazer denúncias",
+                    value: "ticket_denuncia",
+                    emoji: { name: "🥰" },
+                },
+                {
+                    label: "DÚVIDAS",
+                    description: "Selecione para tirar dúvidas",
+                    value: "ticket_duvidas",
+                    emoji: { name: "🫡" },
+                },
+                {
+                    label: "CONTRIBUIÇÃO",
+                    description: "Selecione para fazer contribuições",
+                    value: "ticket_contribuicao",
+                    emoji: { name: "🥶" },
+                },
+                {
+                    label: "OUTROS",
+                    description: "Falar sobre outros assuntos",
+                    value: "ticket_outros",
+                    emoji: { name: "🤬" },
+                },
+            ],
+        },
     },
 };
